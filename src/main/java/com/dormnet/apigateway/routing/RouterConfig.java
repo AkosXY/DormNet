1b19f00b1433b8a06a8b31894f7ff7878cf8da3b
Dittrich Ákos <dittrich.kos@gmail.com>
2024-05-20 00:43:05 +0200

    refactor routing

--- src/main/java/com/dormnet/apigateway/routing/RouterConfig.java
+++ src/main/java/com/dormnet/apigateway/routing/RouterConfig.java
@@ -1,56 +1,56 @@
 package com.dormnet.apigateway.routing;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.web.servlet.function.RequestPredicates.POST;
+import static org.springframework.web.servlet.function.RequestPredicates.path;
 
 @Configuration
 public class RouterConfig {
 
+
     @Bean
-    public RouterFunction<ServerResponse> sportServiceRoute() {
+    public RouterFunction<ServerResponse> sportServiceRoute(@Value("${router.url.sport_service}") String sportServiceUrl) {
         return GatewayRouterFunctions.route("sport_service")
-                .route(RequestPredicates.path("/api/sport"), http("http://localhost:8080"))
-                .route(RequestPredicates.path("/api/sport/{eventId}/add_entries"), http("http://localhost:8080"))
+                .route(path("/api/sport"), http(sportServiceUrl))
+                .route(path("/api/sport/{eventId}/add_entries"), http(sportServiceUrl))
                 .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> reservationServiceRoutes() {
+    public RouterFunction<ServerResponse> reservationServiceRoutes(@Value("${router.url.reservation_service}") String reservationServiceUrl) {
         return GatewayRouterFunctions.route("reservation_service")
-                .route(RequestPredicates.path("/api/reservation/reserve"), http("http://localhost:8081"))
-                .route(RequestPredicates.path("/api/reservation/drop"), http("http://localhost:8081"))
+                .route(path("/api/reservation/reserve"), http(reservationServiceUrl))
+                .route(path("/api/reservation/drop"), http(reservationServiceUrl))
                 .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> resourceServiceRoutes() {
+    public RouterFunction<ServerResponse> resourceServiceRoutes(@Value("${router.url.resource_service}") String resourceServiceUrl) {
         return GatewayRouterFunctions.route("resource_service")
-                .route(RequestPredicates.path("/api/resource/available"), http("http://localhost:8082"))
-                .route(RequestPredicates.POST("/api/resource/makeUnavailable"), http("http://localhost:8082"))
-                .route(RequestPredicates.POST("/api/resource/makeAvailable"), http("http://localhost:8082"))
-                .route(RequestPredicates.POST("/api/resource/admin/add"), http("http://localhost:8082"))
+                .route(path("/api/resource/available"), http(resourceServiceUrl))
+                .route(POST("/api/resource/makeUnavailable"), http(resourceServiceUrl))
+                .route(POST("/api/resource/makeAvailable"), http(resourceServiceUrl))
+                .route(POST("/api/resource/admin/add"), http(resourceServiceUrl))
                 .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> accommodationServiceRoutes() {
+    public RouterFunction<ServerResponse> accommodationServiceRoutes(@Value("${router.url.accommodation_service}") String accommodationServiceUrl) {
         return GatewayRouterFunctions.route("accommodation_service")
-                .route(RequestPredicates.POST("/api/resident"), http("http://localhost:8083"))
-                .route(RequestPredicates.POST("/api/resident/{id}/unassign"), http("http://localhost:8083"))
-                .route(RequestPredicates.POST("/api/resident/{residentId}/assign/{roomId}"), http("http://localhost:8083"))
-
-                .route(RequestPredicates.path("/api/resident"), http("http://localhost:8083"))
-
-                .route(RequestPredicates.POST("/api/rooms"), http("http://localhost:8083"))
-                .route(RequestPredicates.path("/api/rooms"), http("http://localhost:8083"))
+                .route(POST("/api/resident"), http(accommodationServiceUrl))
+                .route(POST("/api/resident/{id}/unassign"), http(accommodationServiceUrl))
+                .route(POST("/api/resident/{residentId}/assign/{roomId}"), http(accommodationServiceUrl))
+                .route(path("/api/resident"), http(accommodationServiceUrl))
+                .route(POST("/api/rooms"), http(accommodationServiceUrl))
+                .route(path("/api/rooms"), http(accommodationServiceUrl))
                 .build();
     }
-
 
 
 }
