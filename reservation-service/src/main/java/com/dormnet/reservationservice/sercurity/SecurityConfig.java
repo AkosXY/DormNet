8243f3be9e519bbfdf323c09711cfa1bd3a6f265
DITTRICH ÁKOS <Akos2.Dittrich@kh.hu>
2025-03-15 23:02:12 +0100

    add basic oauth2 security using keycloak

---
+++ reservation-service/src/main/java/com/dormnet/reservationservice/sercurity/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.dormnet.reservationservice.sercurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests().anyRequest().authenticated();
+        httpSecurity.oauth2ResourceServer().jwt();
+
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return httpSecurity.build();
+    }
+}
